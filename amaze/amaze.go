a80134f041cf22fe4add258871e2cf00c415a79f
Florian Baumann <flo@noqqe.de>
2016-11-30 16:11:31 +0100

    filenames arranged

--- amaze/amaze.go
+++ amaze/amaze.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+  "log"
   "os"
   "os/exec"
-  "log"
-  "encoding/json"
   "strings"
   "time"
+  "encoding/json"
 )
 
+
+// Tiny little configuration struct
 type Config struct {
   hugocmd string
   homedir string
   rvocmd string
 }
 
+// A document coming from rvo
+// looks like this.
 type Document struct {
   Updated struct {
     Date int64 `json:"$date"`
@@ -32,7 +36,7 @@ type Document struct {
   Categories []string `json:"categories"`
 }
 
-func build(c Config) {
+func build(c Config) bool {
   os.Chdir(c.homedir)
   cmd := exec.Command(c.hugocmd)
 
@@ -42,11 +46,16 @@ func build(c Config) {
   }
 
   log.Printf("%s\n", string(output))
+
+  return true
 }
 
 // Create posts for sammelsurium using json export
 // of rvo and persisting them into the posts
-func sammelsurium(c Config) {
+func sammelsurium(c Config) bool {
+
+  // Log message
+  log.Println("Sammelsurium calculating...")
 
   // change directory
   os.Chdir(c.homedir)
@@ -55,10 +64,12 @@ func sammelsurium(c Config) {
   cmdline := strings.Split(c.rvocmd, " ")
   command := cmdline[0]
   args := cmdline[1:]
+
+  // execute commands
   cmd := exec.Command(command, args...)
+  output, err := cmd.CombinedOutput()
 
   // check for errors
-  output, err := cmd.CombinedOutput()
   if err != nil {
     log.Fatal("rvo crashed and burned.")
   }
@@ -68,20 +79,47 @@ func sammelsurium(c Config) {
   var documents []Document
   json.Unmarshal(bytes, &documents)
 
-  // test output
+  log.Printf("Entries found: %d", len(documents))
+
   for d := range documents {
-    convert_timestamp(documents[d].Created.Date)
-    log.Printf("title: %s, created: %d", documents[d].Title, documents[d].Created.Date)
+
+		// dont parse if it has a tag called "private"
+		if stringInSlice("private", documents[d].Tags) {
+			continue
+		}
+
+    log.Printf(generateFilename(documents[d].Title))
+    date := convertTimestamp(documents[d].Created.Date)
+    log.Printf("title: %s, created: %s", documents[d].Title, date)
   }
+
+  return true
+}
+
+func generateFilename(title string) string {
+  name := strings.ToLower(title)
+  name = strings.Replace(name, " ", "-", -1)
+  name = strings.Replace(name, "/", "", -1)
+  name = name + ".md"
+  return name
 }
 
 // convert unix timestamp with 13 chars
 // to human readable date
-func convert_timestamp(ts int64) {
+func convertTimestamp(ts int64) string {
   corr_ts := ts / 1000
-  tm := time.Unix(corr_ts, 0)
-  log.Println(tm.Format(time0)
-  return tm
+  date := time.Unix(corr_ts, 0).Format("2006-01-02 15:04:05")
+  return date
+}
+
+// check if slice contains string
+func stringInSlice(a string, list []string) bool {
+    for _, b := range list {
+        if b == a {
+            return true
+        }
+    }
+    return false
 }
 
 
@@ -94,7 +132,8 @@ func main() {
     hugocmd: "hugo",
     homedir: "/home/noqqe/Code/noqqe.de"}
 
-  log.Println("(Amaze. Wow)Oo.")
+  // Some warm welcome
+  log.Println(".oO(Amaze - Wow)Oo.")
 
   if os.Args[1] == "build" {
     build(c)
