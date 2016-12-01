ff0879276233a80af2dc9160a899edb2e82ea8fe
Florian Baumann <flo@noqqe.de>
2016-12-01 12:24:56 +0100

    header

--- amaze/amaze.go
+++ amaze/amaze.go
@@ -92,16 +92,18 @@ func sammelsurium(c Config) bool {
     // get filename
     filename := generateFilename(documents[d].Title)
 
-    // generate header
 
     // strip content
-    content := removeHeaderFromString(documents[d].Content)
+    content := generateContent(documents[d].Content)
 
     // generate hugo comptabile timestamp
-    date := convertTimestamp(documents[d].Created.Date)
+    date := generateDate(documents[d].Created.Date)
+
+    // generate header
+    header := generateHeader(documents[d].Title, date, documents[d].Tags)
 
     // write sammelsurium post to hugo dir
-    createSammelsuriumFile(content, filename, c)
+    createSammelsuriumFile(c, filename, header, content)
 
     // print debugging
     log.Printf("title: %s, created: %s", documents[d].Title, date)
@@ -111,7 +113,7 @@ func sammelsurium(c Config) bool {
 }
 
 // used to write content as a post to hugo sammelsurium directory
-func createSammelsuriumFile(content string, filename string, c Config) bool {
+func createSammelsuriumFile(c Config, filename string, header string, content string) bool {
 
 	// create file
   f, err := os.Create(c.sammelsuriumdir + "/" + filename)
@@ -119,9 +121,14 @@ func createSammelsuriumFile(content string, filename string, c Config) bool {
       panic(err)
   }
 
-  // convert string to bytes and write to file opened
+  // convert header string to bytes and write to file opened
+  h := []byte(header)
+	f.Write(h)
+
+  // convert header string to bytes and write to file opened
   b := []byte(content)
 	f.Write(b)
+
 	f.Close()
 
   return true
@@ -136,21 +143,54 @@ func generateFilename(title string) string {
   return name
 }
 
-// get rid of the first line of content because
-// it only contains the headline which is not needed
-// in the content anymore.
-func removeHeaderFromString(content string) string {
+func generateContent(content string) string {
+
+  // get rid of the first line of content because
+  // it only contains the headline which is not needed
+  // in the content anymore.
   c := strings.Split(content,"\n")
   c = c[1:]
-  return strings.Join(c[:],"\n")
+  cs := strings.Join(c[:],"\n")
+
+  // replace h1 with h2 for layouting
+  cs = strings.Replace(cs, "# ", "##", -1)
+  return cs
 }
 
 // convert unix timestamp with 13 chars
 // to human readable date
-func convertTimestamp(ts int64) string {
+func generateDate(ts int64) string {
   corr_ts := ts / 1000
   date := time.Unix(corr_ts, 0).Format("2006-01-02 15:04:05")
   return date
+}
+
+// builds hugo header in yaml format
+func generateHeader(title string, date string, tags []string) string {
+  limit := "---"
+  n := "\n"
+
+  // head
+  header := limit + n
+
+  // title
+  header = header + "title: " + title + n
+
+  // date
+  header = header + "date: " + date + n
+
+  // tags
+  if len(tags) > 0 {
+    header = header + "tags: " + n
+    for _, tag := range tags {
+      header = header + "- " + tag + n
+    }
+  }
+
+  // foot
+  header = header + limit + n
+
+  return header
 }
 
 // check if slice contains string
